f506f4e22f452aa36ae34f9f649dae0af85678ac
William Angelico <William Angelico>
2017-08-03 07:38:55 -0300

    automatize sass and reorganize folders

--- gulpfile.js
+++ gulpfile.js
@@ -9,34 +9,54 @@ var browserify  = require('browserify'),
     filter      = require('gulp-filter'),
     concat      = require('gulp-concat'),
     uglify      = require('gulp-uglify');
+    sass        = require('gulp-sass');
 
-
-gulp.task('default', function () {
-  var b = browserify({
-    entries: './src/index.js',
-    debug: true,
-    transform: [babelify.configure({
-      presets: ['react']
-    })]
-  });
-
-  return b.bundle()
-    .pipe(source('ini.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-      // Add other gulp transformations (eg. uglify) to the pipeline here.
-      .on('error', util.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist'));
+gulp.task('react', function () {
+    var b = browserify({
+        entries: './src/scripts/index.js',
+        debug: true,
+        transform: [babelify.configure({
+            presets: ['react']
+        })]
+    });
+    return b.bundle()
+        .pipe(source('ini.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))       
+        .on('error', util.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./src/scripts/bundle'));
 });
-
 
 gulp.task('bower-js', function(){
-   var filterJS = filter(['**/*.js']);
-  return gulp.src('./bower.json')
-    .pipe(mainBowerFiles())
-    .pipe(filterJS)
+    var filterJS = filter(['**/*.js']);
+    return gulp.src('./bower.json')
+        .pipe(mainBowerFiles())
+        .pipe(filterJS)
         .pipe(concat('vendor.js'))
         .pipe(uglify())
-    .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('./src/scripts/bundle'))
 });
+
+gulp.task('js',['bower-js','react'],function(){
+    return gulp.src(['src/scripts/bundle/vendor.js','src/scripts/bundle/ini.js'])
+        .pipe(concat('ini.js'))
+        .pipe(uglify())          
+        .pipe(gulp.dest("./dist/assets/js"));
+});
+
+
+gulp.task('sass', function () {
+  return gulp.src('src/styles/sass/style.scss')    
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('src/styles/css'));
+});
+
+gulp.task('sass-concat',['sass'], function(){
+  return gulp.src('src/styles/css/style.css')
+    .pipe(concat('style.min.css'))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('dist/assets/styles'));
+});
+
+gulp.task('default', ['js','sass-concat']);
